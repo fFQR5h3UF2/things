442349f7cc09c411d24e645158cadff2911953f5
q1feq3qzi60u <97828377+q1feq3qzi60u@users.noreply.github.com>
2023-12-23 04:59:29 +0300

    ci: move make installation to ci

--- scripts/test_makefile.sh
+++ scripts/test_makefile.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuxo pipefail
 
+which make || apt-get install -y build-essential
 make setup_repo setup_packages home git firefox vscode nvim update_readme
 . "${HOME}/.bashrc"
 __source_scripts ssdfsdf "${HOME}/.bashrc"
