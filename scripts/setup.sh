5ee8f14edddbf902d2c9f7b1b1f4b58977a0d894
q1feq3qzi60u <97828377+q1feq3qzi60u@users.noreply.github.com>
2023-12-30 15:47:33 +0300

    feat(setup): add fzf to packages

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,34 +1,34 @@
 #!/usr/bin/env bash
 set -Eeuxo pipefail
 
-mkdir -p ~/.local/bin ~/repos ~/.ssh ~/.gnupg
+dotfiles_dir=~/repos/shishifubing/dotfiles
+dotfiles_url=https://github.com/shishifubing/dotfiles.git
+
+mkdir -p "${dotfiles_dir}" ~/.local/bin ~/.ssh ~/.gnupg
 chmod 0700 ~/.ssh ~/.gnupg
 
-dotfiles_dir="${HOME}/repos/shishifubing/dotfiles"
-dotfiles_url="https://github.com/shishifubing/dotfiles.git"
-mkdir -p "${dotfiles_dir}"
 if [[ -d "${dotfiles_dir}/.git" ]]; then
-	git --git-dir "${dotfiles_dir}/.git" checkout main
-	git --git-dir "${dotfiles_dir}/.git" pull
+    git --git-dir "${dotfiles_dir}/.git" checkout main
+    git --git-dir "${dotfiles_dir}/.git" pull
 else
-	git clone "${dotfiles_url}" "${dotfiles_dir}"
+    git clone "${dotfiles_url}" "${dotfiles_dir}"
 fi
 cd "${dotfiles_dir}"
 
 packages=(
-	make git curl wget unzip stow gpg python3 python3-pip python3-venv
-	gpg-agent shellcheck shfmt sudo
+    make git curl wget unzip stow gpg python3 python3-pip python3-venv
+    gpg-agent shellcheck shfmt sudo fzf unzip tar
 )
 if [[ "$(whoami)" == "root" ]]; then
-	apt-get update
-	apt-get install -y "${packages[@]}"
+    apt-get update
+    apt-get install -y "${packages[@]}"
 else
-	sudo apt-get update
-	sudo apt-get install -y "${packages[@]}"
+    sudo apt-get update
+    sudo apt-get install -y "${packages[@]}"
 fi
 
 python3 -m venv --symlinks --clear --upgrade-deps --prompt 'home' ~/.venv
-# shellcheck source=/dev/null.
+# shellcheck source=/dev/null
 . ~/.venv/bin/activate
 pip install commitizen=="3.13.0" pre-commit=="3.6.0"
 cz version --project
@@ -62,47 +62,44 @@ glab_distrib="glab_${glab_version}_Linux_x86_64.tar.gz"
 protoc_distrib="protoc-${protoc_version}-linux-x86_64.zip"
 
 urls=(
-	"${hashicorp_url}/terraform/${terraform_version}/${terraform_distrib}"
-	"${hashicorp_url}/packer/${packer_version}/${packer_distrib}"
-	"${gitversion_url}/${gitversion_version}/${gitversion_distrib}"
-	"${k8s_url}/${kubectl_version}/bin/linux/amd64/kubectl"
-	"${yc_url}/release/${yc_version}/linux/amd64/yc"
-	"${terraform_docs_url}/${terraform_docs_version}/${terraform_docs_distrib}"
-	"${helm_url}/${helm_distrib}"
-	"${glab_url}/v${glab_version}/downloads/${glab_distrib}"
-	"${protoc_url}/v${protoc_version}/${protoc_distrib}"
+    "${hashicorp_url}/terraform/${terraform_version}/${terraform_distrib}"
+    "${hashicorp_url}/packer/${packer_version}/${packer_distrib}"
+    "${gitversion_url}/${gitversion_version}/${gitversion_distrib}"
+    "${k8s_url}/${kubectl_version}/bin/linux/amd64/kubectl"
+    "${yc_url}/release/${yc_version}/linux/amd64/yc"
+    "${terraform_docs_url}/${terraform_docs_version}/${terraform_docs_distrib}"
+    "${helm_url}/${helm_distrib}"
+    "${glab_url}/v${glab_version}/downloads/${glab_distrib}"
+    "${protoc_url}/v${protoc_version}/${protoc_distrib}"
 )
 binaries=(
-	"terraform" "packer" "gitversion" "kubectl" "yc" "helm" "terraform-docs"
-	"glab" "protoc"
+    "terraform" "packer" "gitversion" "kubectl" "yc" "helm" "terraform-docs"
+    "glab" "protoc"
 )
 
-echo "prepare the work directory"
 temp=$(mktemp -d)
-
 exit_code=0
 (
-	cd "${temp}"
-	echo "downloading"
-	for url in "${urls[@]}"; do
-		wget --no-verbose "${url}"
-	done
+    cd "${temp}"
+    echo "downloading"
+    for url in "${urls[@]}"; do
+        wget --no-verbose "${url}"
+    done
 
-	echo "unzipping"
-	for zip in *.zip; do
-		unzip "${zip}"
-	done
-	for tar in *.tar.gz; do
-		tar -xvzf "${tar}"
-	done
-	mv ./*/helm ./bin/* ./
+    echo "unzipping"
+    for zip in *.zip; do
+        unzip "${zip}"
+    done
+    for tar in *.tar.gz; do
+        tar -xvzf "${tar}"
+    done
+    mv ./*/helm ./bin/* ./
 
-	echo "installing binaries"
-	chmod +x "${binaries[@]}"
-	mv "${binaries[@]}" ~/.local/bin/
+    echo "installing binaries"
+    chmod +x "${binaries[@]}"
+    mv "${binaries[@]}" ~/.local/bin/
 ) || {
-	exit_code="${?}"
+    exit_code="${?}"
 }
-
 rm -rf "${temp}"
 exit "${exit_code}"
