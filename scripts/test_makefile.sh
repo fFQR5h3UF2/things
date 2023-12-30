6766e7730a6d83ceb08fbce733f145f73bb23922
q1feq3qzi60u <97828377+q1feq3qzi60u@users.noreply.github.com>
2023-12-30 19:08:00 +0000

    feat(nvim): add ruler, add stylua hook, enable autorefactor (#25)
    * feat(nvim): add leader and stylua hook, enable autorefactor
    
    * ci: use reusable workflow on main
    
    * test(precommit): add github action checks
    
    * fix(test_makefile): fix script path

--- scripts/test_makefile.sh
+++ scripts/test_makefile.sh
@@ -3,4 +3,4 @@ set -Eeuxo pipefail
 
 make setup home git firefox vscode nvim update_readme
 . scripts/bashrc.sh
-__source_scripts ssdfsdf "${HOME}/.bashrc"
+_dotfiles_source_scripts ssdfsdf "${HOME}/.bashrc"
