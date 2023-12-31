8ff5d6ad3fc3d27a3b68bff160f05d0b0d994e43
q1feq3qzi60u <97828377+q1feq3qzi60u@users.noreply.github.com>
2023-12-31 18:04:45 +0300

    refactor(nvim): move keymaps to another file

--- nvim/.config/nvim/lua/shishifubing/init.lua
+++ nvim/.config/nvim/lua/shishifubing/init.lua
@@ -28,62 +28,15 @@ vim.opt.rtp:prepend(lazypath)
 --    as they will be available in your neovim runtime.
 require("lazy").setup({ { import = "shishifubing.plugins" } }, {})
 require("shishifubing.options")
-
--- [[ Basic Keymaps ]]
-
--- Keymaps for better default experience
--- See `:help vim.keymap.set()`
-vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
-
--- Remap for dealing with word wrap
-vim.keymap.set(
-    "n",
-    "k",
-    "v:count == 0 ? 'gk' : 'k'",
-    { expr = true, silent = true }
-)
-vim.keymap.set(
-    "n",
-    "j",
-    "v:count == 0 ? 'gj' : 'j'",
-    { expr = true, silent = true }
-)
-
--- Diagnostic keymaps
-vim.keymap.set(
-    "n",
-    "[d",
-    vim.diagnostic.goto_prev,
-    { desc = "Go to previous diagnostic message" }
-)
-vim.keymap.set(
-    "n",
-    "]d",
-    vim.diagnostic.goto_next,
-    { desc = "Go to next diagnostic message" }
-)
-vim.keymap.set(
-    "n",
-    "<leader>e",
-    vim.diagnostic.open_float,
-    { desc = "Open floating diagnostic message" }
-)
-vim.keymap.set(
-    "n",
-    "<leader>q",
-    vim.diagnostic.setloclist,
-    { desc = "Open diagnostics list" }
-)
+require("shishifubing.keymaps")
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
-local highlight_group =
-    vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
     callback = function()
         vim.highlight.on_yank()
     end,
-    group = highlight_group,
+    group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
     pattern = "*",
 })
 
@@ -127,92 +80,6 @@ local function live_grep_git_root()
         })
     end
 end
-
-vim.api.nvim_create_user_command("LiveGrepGitRoot", live_grep_git_root, {})
-
--- See `:help telescope.builtin`
-vim.keymap.set(
-    "n",
-    "<leader>sr",
-    require("telescope.builtin").oldfiles,
-    { desc = "[S]earch [R]ecently opened files" }
-)
-vim.keymap.set(
-    "n",
-    "<leader><space>",
-    require("telescope.builtin").buffers,
-    { desc = "[ ] Find existing buffers" }
-)
-vim.keymap.set("n", "<leader>/", function()
-    -- You can pass additional configuration to telescope to change theme, layout, etc.
-    require("telescope.builtin").current_buffer_fuzzy_find(
-        require("telescope.themes").get_dropdown({
-            winblend = 10,
-            previewer = false,
-        })
-    )
-end, { desc = "[/] Fuzzily search in current buffer" })
-
-vim.keymap.set("n", "<leader>s/", function()
-    require("telescope.builtin").live_grep({
-        grep_open_files = true,
-        prompt_title = "Live Grep in Open Files",
-    })
-end, { desc = "[S]earch [/] in Open Files" })
-vim.keymap.set(
-    "n",
-    "<leader>ss",
-    require("telescope.builtin").builtin,
-    { desc = "[S]earch [S]elect Telescope" }
-)
-vim.keymap.set(
-    "n",
-    "<leader>?",
-    require("telescope.builtin").git_files,
-    { desc = "[?] Search Git Files" }
-)
-vim.keymap.set(
-    "n",
-    "<leader>sf",
-    require("telescope.builtin").find_files,
-    { desc = "[S]earch [F]iles" }
-)
-vim.keymap.set(
-    "n",
-    "<leader>sh",
-    require("telescope.builtin").help_tags,
-    { desc = "[S]earch [H]elp" }
-)
-vim.keymap.set(
-    "n",
-    "<leader>sw",
-    require("telescope.builtin").grep_string,
-    { desc = "[S]earch current [W]ord" }
-)
-vim.keymap.set(
-    "n",
-    "<leader>sg",
-    require("telescope.builtin").live_grep,
-    { desc = "[S]earch by [G]rep" }
-)
-vim.keymap.set(
-    "n",
-    "<leader>sG",
-    ":LiveGrepGitRoot<cr>",
-    { desc = "[S]earch by [G]rep on Git Root" }
-)
-vim.keymap.set(
-    "n",
-    "<leader>sd",
-    require("telescope.builtin").diagnostics,
-    { desc = "[S]earch [D]iagnostics" }
-)
-vim.keymap.set(
-    "n",
-    "<leader>sR",
-    require("telescope.builtin").resume,
-    { desc = "[S]earch [R]esume" }
-)
 
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
@@ -363,6 +230,3 @@ mason_lspconfig.setup_handlers({
         })
     end,
 })
-
--- [[ Configure nvim-cmp ]]
--- See `:help cmp`
