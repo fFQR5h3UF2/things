4b373eb0a96a0ee78fcc4e0954e00309e3c1fa2a
q1feq3qzi60u <97828377+q1feq3qzi60u@users.noreply.github.com>
2024-01-01 16:50:02 +0300

    refactor(nvim): split init file further

--- nvim/.config/nvim/lua/shishifubing/init.lua
+++ nvim/.config/nvim/lua/shishifubing/init.lua
@@ -4,182 +4,10 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- [[ Install `lazy.nvim` plugin manager ]]
---    https://github.com/folke/lazy.nvim
---    `:help lazy.nvim.txt` for more info
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
-end
-vim.opt.rtp:prepend(lazypath)
-
--- [[ Configure plugins ]]
--- NOTE: Here is where you install your plugins.
---  You can configure plugins using the `config` key.
---
---  You can also configure plugins after the setup call,
---    as they will be available in your neovim runtime.
-require("lazy").setup({ { import = "shishifubing.plugins" } }, {})
+require("shishifubing.lazy")
 require("shishifubing.options")
 require("shishifubing.keymaps")
-
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd("TextYankPost", {
-    callback = function()
-        vim.highlight.on_yank()
-    end,
-    group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
-    pattern = "*",
-})
-
--- Enable telescope fzf native, if installed
-pcall(require("telescope").load_extension, "fzf")
-
--- Telescope live_grep in git root
--- Function to find the git root directory based on the current buffer's path
-local function find_git_root()
-    -- Use the current buffer's path as the starting point for the git search
-    local current_file = vim.api.nvim_buf_get_name(0)
-    local current_dir
-    local cwd = vim.fn.getcwd()
-    -- If the buffer is not associated with a file, return nil
-    if current_file == "" then
-        current_dir = cwd
-    else
-        -- Extract the directory from the current file's path
-        current_dir = vim.fn.fnamemodify(current_file, ":h")
-    end
-
-    -- Find the Git root directory from the current file's path
-    local git_root = vim.fn.systemlist(
-        "git -C "
-            .. vim.fn.escape(current_dir, " ")
-            .. " rev-parse --show-toplevel"
-    )[1]
-    if vim.v.shell_error ~= 0 then
-        print("Not a git repository. Searching on current working directory")
-        return cwd
-    end
-    return git_root
-end
-
--- Custom live_grep function to search in git root
-local function live_grep_git_root()
-    local git_root = find_git_root()
-    if git_root then
-        require("telescope.builtin").live_grep({
-            search_dirs = { git_root },
-        })
-    end
-end
-
--- [[ Configure LSP ]]
---  This function gets run when an LSP connects to a particular buffer.
-local on_attach = function(_, bufnr)
-    -- NOTE: Remember that lua is a real programming language, and as such it is possible
-    -- to define small helper and utility functions so you don't have to repeat yourself
-    -- many times.
-    --
-    -- In this case, we create a function that lets us more easily define mappings specific
-    -- for LSP related items. It sets the mode, buffer and description for us each time.
-    local nmap = function(keys, func, desc)
-        if desc then
-            desc = "LSP: " .. desc
-        end
-
-        vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-    end
-
-    nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-    nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-
-    nmap(
-        "gd",
-        require("telescope.builtin").lsp_definitions,
-        "[G]oto [D]efinition"
-    )
-    nmap(
-        "gr",
-        require("telescope.builtin").lsp_references,
-        "[G]oto [R]eferences"
-    )
-    nmap(
-        "gI",
-        require("telescope.builtin").lsp_implementations,
-        "[G]oto [I]mplementation"
-    )
-    nmap(
-        "<leader>D",
-        require("telescope.builtin").lsp_type_definitions,
-        "Type [D]efinition"
-    )
-    nmap(
-        "<leader>ds",
-        require("telescope.builtin").lsp_document_symbols,
-        "[D]ocument [S]ymbols"
-    )
-    nmap(
-        "<leader>ws",
-        require("telescope.builtin").lsp_dynamic_workspace_symbols,
-        "[W]orkspace [S]ymbols"
-    )
-
-    -- See `:help K` for why this keymap
-    nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-    nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-
-    -- Lesser used LSP functionality
-    nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-    nmap(
-        "<leader>wa",
-        vim.lsp.buf.add_workspace_folder,
-        "[W]orkspace [A]dd Folder"
-    )
-    nmap(
-        "<leader>wr",
-        vim.lsp.buf.remove_workspace_folder,
-        "[W]orkspace [R]emove Folder"
-    )
-    nmap("<leader>wl", function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, "[W]orkspace [L]ist Folders")
-
-    -- Create a command `:Format` local to the LSP buffer
-    vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-        vim.lsp.buf.format()
-    end, { desc = "Format current buffer with LSP" })
-end
-
--- document existing key chains
-require("which-key").register({
-    ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-    ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-    ["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
-    ["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
-    ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-    ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-    ["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-    ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-})
--- register which-key VISUAL mode
--- required for visual <leader>hs (hunk stage) to work
-require("which-key").register({
-    ["<leader>"] = { name = "VISUAL <leader>" },
-    ["<leader>h"] = { "Git [H]unk" },
-}, { mode = "v" })
-
--- mason-lspconfig requires that these setup functions are called in this order
--- before setting up the servers.
-require("mason").setup()
-require("mason-lspconfig").setup()
+require("shishifubing.autocmd")
 
 -- Enable the following language servers
 --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -206,9 +34,6 @@ local servers = {
     },
 }
 
--- Setup neovim lua configuration
-require("neodev").setup()
-
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
@@ -224,7 +49,7 @@ mason_lspconfig.setup_handlers({
     function(server_name)
         require("lspconfig")[server_name].setup({
             capabilities = capabilities,
-            on_attach = on_attach,
+            on_attach = LSP_on_buffer_attach,
             settings = servers[server_name],
             filetypes = (servers[server_name] or {}).filetypes,
         })
