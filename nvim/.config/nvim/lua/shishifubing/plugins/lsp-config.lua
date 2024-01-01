6005c10464e090a7d5b99d2daba36488a2ec6b13
q1feq3qzi60u <97828377+q1feq3qzi60u@users.noreply.github.com>
2024-01-01 14:37:25 +0000

    refactor(nvim): move everything out of the init file (#36)

--- nvim/.config/nvim/lua/shishifubing/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/shishifubing/plugins/lsp-config.lua
@@ -1,32 +1,16 @@
 return {
-
     {
         -- LSP Configuration & Plugins
         "neovim/nvim-lspconfig",
         dependencies = {
             -- Automatically install LSPs to stdpath for neovim
-            { "williamboman/mason.nvim", config = true, opts = {} },
+            { "williamboman/mason.nvim", opts = {} },
             { "williamboman/mason-lspconfig.nvim", opts = {} },
-
             -- Useful status updates for LSP
-            -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
             { "j-hui/fidget.nvim", opts = {} },
-
             -- Additional lua configuration, makes nvim stuff amazing!
             { "folke/neodev.nvim", opts = {} },
         },
-        opts = {
-            servers = {
-                yamlls = {
-                    settings = {
-                        yaml = {
-                            customTags = {
-                                "!reference sequence",
-                            },
-                        },
-                    },
-                },
-            },
-        },
+        opts = {},
     },
 }
