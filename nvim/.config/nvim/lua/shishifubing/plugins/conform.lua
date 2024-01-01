2dbae3921c47e705d22f06863ed9079348bb5e9a
q1feq3qzi60u <97828377+q1feq3qzi60u@users.noreply.github.com>
2024-01-01 15:06:54 +0300

    feat(nvim): switch to conform from nullls

--- nvim/.config/nvim/lua/shishifubing/plugins/conform.lua
+++ nvim/.config/nvim/lua/shishifubing/plugins/conform.lua
@@ -1,16 +1,40 @@
 return {
     {
-        "jose-elias-alvarez/null-ls.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            local null_ls = require("null-ls")
-
-            null_ls.setup({
-                sources = {
-                    null_ls.builtins.diagnostics.ruff,
-                    null_ls.builtins.formatting.black,
+        "stevearc/conform.nvim",
+        dependencies = {
+            { "neovim/nvim-lspconfig" },
+            { "nvim-lua/plenary.nvim" },
+            { "williamboman/mason.nvim" },
+        },
+        opts = {
+            formatters_by_ft = {
+                lua = { "stylua" },
+                python = { "isort", "black" },
+                bash = { "shfmt" },
+                sh = { "shfmt" },
+                go = { "goimports", "gofumpt", "goimports-reviser" },
+                javascript = { { "prettierd", "prettier" } },
+                typescript = { { "prettierd", "prettier" } },
+                vue = { { "prettierd", "prettier" } },
+                css = { { "prettierd", "prettier" } },
+                scss = { { "prettierd", "prettier" } },
+                less = { { "prettierd", "prettier" } },
+                html = { { "prettierd", "prettier" } },
+                json = { { "prettierd", "prettier" } },
+                jsonc = { { "prettierd", "prettier" } },
+                yaml = { { "prettierd", "prettier" } },
+                markdown = { { "prettierd", "prettier" } },
+            },
+            format_on_save = {
+                -- These options will be passed to conform.format()
+                timeout_ms = 500,
+                lsp_fallback = true,
+            },
+            formatters = {
+                shfmt = {
+                    prepend_args = { "--indent=4" },
                 },
-            })
-        end,
+            },
+        },
     },
 }
