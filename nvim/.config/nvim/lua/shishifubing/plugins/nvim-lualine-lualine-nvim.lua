3f84ebd0e5d9b1d00316fa1cbc22aaaaa7527340
q1feq3qzi60u <97828377+q1feq3qzi60u@users.noreply.github.com>
2024-01-02 16:44:27 +0300

    feat(nvim): add bufferline

--- nvim/.config/nvim/lua/shishifubing/plugins/nvim-lualine-lualine-nvim.lua
+++ nvim/.config/nvim/lua/shishifubing/plugins/nvim-lualine-lualine-nvim.lua
@@ -1,14 +1,10 @@
 return {
     {
-        -- Set lualine as statusline
         "nvim-lualine/lualine.nvim",
-        -- See `:help lualine.txt`
         opts = {
             options = {
                 theme = "tokyonight",
                 icons_enabled = false,
-                component_separators = "|",
-                section_separators = "",
             },
         },
     },
