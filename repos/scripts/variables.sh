dfa4d394c817f5a80347a1f5e660c1ff1a13902b
q1feq3qzi60u <97828377+q1feq3qzi60u@users.noreply.github.com>
2024-01-04 01:35:49 +0300

    feat(repos): move infra-repos to this repository

---
+++ repos/scripts/variables.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+GITHUB_TOKEN=$(
+    secret-tool lookup github "[repo, admin:org, workflow] personal"
+)
+GITLAB_TOKEN=$(secret-tool lookup gitlab "[api] personal")
+AWS_ACCESS_KEY_ID=$(secret-tool lookup bucket-terraform-admin-personal access-key)
+AWS_SECRET_ACCESS_KEY=$(secret-tool lookup bucket-terraform-admin-personal secret-key)
+
+export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY GITHUB_TOKEN GITLAB_TOKEN
