fdd1198a03b55bee84650b8e3cca85073b8ecab7
q1feq3qzi60u <97828377+q1feq3qzi60u@users.noreply.github.com>
2024-01-04 16:42:26 +0300

    feat(nvim): move all configuration inside lua/shishifubing

--- stow/nvim/.config/nvim/init.lua
+++ stow/nvim/.config/nvim/init.lua
@@ -1,6 +1,1 @@
-require("shishifubing.mapleader")
-require("shishifubing.lazy")
-require("shishifubing.options")
-require("shishifubing.autocmd")
-require("shishifubing.filetypes")
-require("shishifubing.keymaps").set_no_deps()
+require("shishifubing")
