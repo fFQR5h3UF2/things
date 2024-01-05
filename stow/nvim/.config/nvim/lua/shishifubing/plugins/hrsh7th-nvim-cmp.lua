6135e5f0e11eb13fffe82cc68519dbafaa61d88a
q1feq3qzi60u <97828377+q1feq3qzi60u@users.noreply.github.com>
2024-01-05 10:48:15 +0000

    feat(nvim): add snippets (#67)

--- stow/nvim/.config/nvim/lua/shishifubing/plugins/hrsh7th-nvim-cmp.lua
+++ stow/nvim/.config/nvim/lua/shishifubing/plugins/hrsh7th-nvim-cmp.lua
@@ -4,21 +4,22 @@ return {
         "hrsh7th/nvim-cmp",
         dependencies = {
             -- Snippet Engine & its associated nvim-cmp source
-            "L3MON4D3/LuaSnip",
-            "saadparwaiz1/cmp_luasnip",
+            { "L3MON4D3/LuaSnip" },
+            { "saadparwaiz1/cmp_luasnip" },
 
             -- Adds LSP completion capabilities
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-path",
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-path" },
 
             -- Adds a number of user-friendly snippets
-            "rafamadriz/friendly-snippets",
+            { "rafamadriz/friendly-snippets" },
         },
         config = function()
             local cmp = require("cmp")
             local luasnip = require("luasnip")
 
             require("luasnip.loaders.from_vscode").lazy_load()
+            require("luasnip.loaders.from_snipmate").lazy_load()
             luasnip.config.setup({})
 
             cmp.setup({
