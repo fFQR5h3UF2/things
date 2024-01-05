a3b0add63d542098a3e518f595383dd3ba3cb476
q1feq3qzi60u <97828377+q1feq3qzi60u@users.noreply.github.com>
2024-01-05 14:35:00 +0300

    feat(nvim): remove bufferline

--- stow/nvim/.config/nvim/lua/shishifubing/plugins/nvim-lualine-lualine-nvim.lua
+++ stow/nvim/.config/nvim/lua/shishifubing/plugins/nvim-lualine-lualine-nvim.lua
@@ -3,6 +3,8 @@ return {
         "nvim-lualine/lualine.nvim",
         opts = {
             options = {
+                globalstatus = true,
+                theme = "tokyonight",
                 icons_enabled = false,
                 component_separators = "",
                 section_separators = "",
