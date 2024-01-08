70fdc8c2e7aedf54f1479394b0e69ecb1de45617
q1feq3qzi60u <97828377+q1feq3qzi60u@users.noreply.github.com>
2024-01-08 11:50:23 +0000

    feat(nvim): switch to sonokai theme (#76)

---
+++ nvim/.config/nvim/lua/shishifubing/plugins/sainnhe-sonokai.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        "sainnhe/sonokai",
+        lazy = false,
+        priority = 1000,
+        enabled = true,
+        config = function()
+            vim.g.sonokai_style = "default"
+            vim.g.sonokai_colors_override = {
+                bg0 = { "#333333", "235" },
+            }
+            vim.cmd.colorscheme("sonokai")
+        end,
+    },
+}
