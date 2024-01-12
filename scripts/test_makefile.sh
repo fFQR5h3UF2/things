7bfff9e41e5ef91aec21aeea7111dcc61fde812e
q1feq3qzi60u <97828377+q1feq3qzi60u@users.noreply.github.com>
2024-01-12 22:11:04 +0300

    test: fix function name

--- scripts/test_makefile.sh
+++ scripts/test_makefile.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
-set -Eeuxo pipefail
+#!/usr/bin/env sh
+set -o errexit -o nounset -o xtrace
 
 export DOTFILES="${PWD}"
 make setup git firefox vscode update_readme
 . scripts/bashrc.sh
-_dotfiles_source_scripts ssdfsdf "${HOME}/.bashrc"
+dotfiles_source_scripts ssdfsdf "${HOME}/.bashrc"
