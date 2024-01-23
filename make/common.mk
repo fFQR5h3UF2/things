1406988e983ffc858531da9040a0d6f20efc764d
q1feq3qzi60u <97828377+q1feq3qzi60u@users.noreply.github.com>
2024-01-23 17:46:24 +0000

    feat(ssh): move ssh setup to a separate package (#102)

--- make/common.mk
+++ make/common.mk
@@ -1,4 +1,4 @@
-OUT_DIR = out
+OUT_DIR = ./out
 TRACKER_DIR = ${OUT_DIR}/tracker
 STOW_PACKAGE = stow
 STOW_OUT_DIR = ${OUT_DIR}/${STOW_PACKAGE}
@@ -9,19 +9,27 @@ STOW_BUILD = $(STOW_CMD) --target "${STOW_OUT_DIR}"
 STOW_INSTALL = $(STOW_CMD) --target "${STOW_INSTALL_TARGET}" --dir "${OUT_DIR}"
 OUT_DIRS = ${OUT_DIR} ${TRACKER_DIR} ${STOW_OUT_DIR}
 
-.PHONY: install install-stow
-.PHONY: clean clean-out clean-stow
-.PHONY: build build-stow
-.PHONY: test test-stow
-.PHONY: update
-.PHONY: setup
+define clean
+	if [ -e "${1}" ]; then rm -rf "${1}"; fi
+endef
 
 install: build
+.PHONY: install
+
 setup: | $(OUT_DIRS)
+.PHONY: setup
+
 build: setup
+.PHONY: build
+
 test: build
+.PHONY: test
+
 update:
-clean: clean-out
+.PHONY: update
+
+clean: clean-out clean-venv
+.PHONY: clean
 
 ${TRACKER_DIR}/%:
 	touch "${@}"
@@ -32,19 +40,42 @@ $(OUT_DIR):
 ${OUT_DIR}/%:
 	mkdir -p "${@}"
 
-build-stow: ${TRACKER_DIR}/build-stow | setup
-${TRACKER_DIR}/build-stow: $(STOW_FILES)
+setup-poetry-env:
+	poetry env use python3
+.PHONY: setup-poetry-env
+
+setup-poetry: setup-poetry-env
+	poetry install
+.PHONY: setup-poetry
+
+setup-poetry-no-root: setup-poetry-env
+	poetry install --no-root
+.PHONY: setup-poetry-no-root
+
+setup-ansible-galaxy: setup-poetry-no-root
+	poetry run ansible-galaxy install -r requirements.yml
+.PHONY: setup-ansible-galaxy
+
+build-stow: setup
 	$(STOW_BUILD) --stow "${STOW_PACKAGE}"
-	touch "${@}"
+.PHONY: build-stow
 
 install-stow: build
 	$(STOW_INSTALL) --stow "${STOW_PACKAGE}"
+.PHONY: install-stow
 
 test-stow: build
 	$(STOW_INSTALL) --simulate "${STOW_PACKAGE}"
+.PHONY: test-stow
 
 clean-stow:
 	if [ -d "${STOW_OUT_DIR}" ]; then $(STOW_INSTALL) --delete "${STOW_PACKAGE}"; fi
+.PHONY: clean-stow
 
 clean-out: clean-stow
-	if [ -e "${OUT_DIR}" ]; then rm -r "${OUT_DIR}"; fi
+	$(call clean,${OUT_DIR})
+.PHONY: clean-out
+
+clean-venv:
+	poetry env remove --all
+.PHONY: clean-venv
