b9e1412c203b1bddb3e406b47e0fa7ce50c72592
q1feq3qzi60u <97828377+q1feq3qzi60u@users.noreply.github.com>
2024-02-05 20:10:20 +0300

    feat: remove ci files

--- ci/actions/bump_version/test_bump_version_commit.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -Eeuxo pipefail
-git config --global user.email "sdfsadf"
-git config --global user.name "asdfsdfs"
-git commit --allow-empty -m "feat: empty commit to force bump"
