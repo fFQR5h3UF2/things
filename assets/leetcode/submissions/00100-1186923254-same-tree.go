298a18f7977098c7312bcb271f5ebd681b919a10
q1feq3qzi60u <97828377+q1feq3qzi60u@users.noreply.github.com>
2024-02-26 20:12:20 +0300

    feat(leetcode): add 24-02-26 submissions

---
+++ assets/leetcode/submissions/00100-1186923254-same-tree.go
@@ -0,0 +1,22 @@
+// Submission title: Same Tree
+// Submission url  : https://leetcode.com/problems/same-tree/description/
+// Submission url  : https://leetcode.com/submissions/detail/1186923254/"
+package submissions
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil || p.Val != q.Val {
+		return false
+	}
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
