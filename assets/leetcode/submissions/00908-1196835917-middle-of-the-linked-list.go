bfed8e4fc624131014f251e6ffd8aedd3b1da92f
q1feq3qzi60u <97828377+q1feq3qzi60u@users.noreply.github.com>
2024-03-07 16:10:44 +0000

    chore(leetcode): add 24-03-07 submissions (#231)

---
+++ assets/leetcode/submissions/00908-1196835917-middle-of-the-linked-list.go
@@ -0,0 +1,32 @@
+// Submission title: Middle of the Linked List
+// Submission url  : https://leetcode.com/problems/middle-of-the-linked-list/description/
+// Submission url  : https://leetcode.com/submissions/detail/1196835917/"
+package submissions
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func middleNode(head *ListNode) *ListNode {
+	if head.Next == nil {
+		return head
+	}
+	slow, fast := head, head.Next
+	move := true
+	for fast.Next != nil {
+		if move {
+			slow = slow.Next
+			move = false
+		} else {
+			move = true
+		}
+		fast = fast.Next
+	}
+	if move {
+		return slow.Next
+	}
+	return slow
+}
